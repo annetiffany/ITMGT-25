0b507ba4b2780ecbefc2e782d11c2102e5df8e98
annetiffany <135828964+annetiffany@users.noreply.github.com>
2023-06-09 16:10:33 +0800

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/demo-checkpoint.py
+++
@@ -1,7 +0,0 @@
-print ("hello world")
-def savings(gross_pay,tax_rate,expenses,wow):
-    rawr = gross_pay-round(gross_pay*tax_rate)-expenses
-    return str(rawr) + wow
-print ("hello world")
-a = savings(1000,0.10,100,"kg")
-print(a)
